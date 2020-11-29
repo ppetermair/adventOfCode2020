60d99b4687f6e82641ba5b279a4a0c4885765fe6
Patrick Petermair <patrick.petermair@gmail.com>
2020-11-29 18:31:51 +0100

    finish initializer and template

--- initDay.py
+++ initDay.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 import os
+import shutil
 
 day = str(sys.argv[1])
 directory = 'day' + day
@@ -11,10 +12,10 @@ os.mkdir(directory)
 cookie_value = open('cookie').read().replace('\n', '')
 cookies = {'session': cookie_value}
 
-print(cookies)
-
 url = 'https://adventofcode.com/2020/day/{}/input'.format(day)
 respone = requests.get(url, cookies=cookies)
 
 path = os.path.join(directory, 'input')
 open(path, 'wb').write(respone.content)
+
+shutil.copy('solution_template.py', os.path.join(directory, 'solution.py'))
